98a6a31e471adac9a61c3b77ddef151bb467330a
Thomas de Jong <thomas@tdejong.io>
2022-02-12 05:38:55 +0100

    Fixed some bugs in the v2 of import_data

--- import_data_v2.py
+++ import_data_v2.py
@@ -4,27 +4,27 @@ from app.models import Companies, Cities, Meta, companies_meta
 
 
 # Function to insert meta data
-# Example insert_meta(agency['disciplines'], 'Discipline')
-def insert_meta(input, type):
+# Example insert_meta(agency['disciplines'], 'Discipline', company_id)
+def insert_meta(input, type, company_id):
     if input is not None:
-        # Iterate over the disciplines
+        # Iterate over the meta type
         for item in input:
 
-            # Check if the discipline is already in the Meta table
+            # Check if the meta string is already in the Meta table
             item_check = Meta.query.filter_by(
                 type=type, meta_string=item).first()
 
-            # Add the discipline if not in Meta table
+            # Add the meta string if not in Meta table
             if item_check is None:
                 item_input = Meta(type=type, meta_string=item)
                 db.session.add(item_input)
                 db.session.commit()
 
-                # Query the newly added discipline from the Meta table
+                # Query the newly added meta string from the Meta table
                 item_check = Meta.query.filter_by(
                     type=type, meta_string=item).first()
 
-            # Get the meta_id for the companies_meta table if the discipline was already in the table or just added to the table
+            # Get the meta_id for the companies_meta table if the meta string was already in the table or just added to the table
             meta_id = item_check.meta_id
 
             # Add the company_id and meta_id to the companies_meta table
@@ -48,45 +48,34 @@ with open(import_file) as file:
     for agency in data['agencies']:
 
         # Check if the city is already in the Cities table
-        city_name = agency['city']
-        city_check = Cities.query.filter_by(city_name=city_name).first()
+        city_check = Cities.query.filter_by(city_name=agency['city']).first()
 
         # Add the city if city not in Cities table
         if city_check is None:
 
-            # Get the values for the Cities table
-            # city_name = agency['city']
-            region = agency['region']
-
             # Create the insert for the Cities table and add it
-            city_insert = Cities(city_name=city_name, region=region)
+            city_insert = Cities(
+                city_name=agency['city'], region=agency['region'])
             db.session.add(city_insert)
             db.session.commit()
 
             # Query the newly added city from the Cities table
-            city_check = Cities.query.filter_by(city_name=city_name).first()
+            city_check = Cities.query.filter_by(
+                city_name=agency['city']).first()
 
         # Get the city_id for the Companies table if the city was already in the table or just added to the table
         city_id = city_check.city_id
 
-        # Get the values for the Companies table
-        company_id = agency['id']
-        company_name = agency['name']
-        logo_image_src = agency['eguideImageSrc']
-        website = agency['website']
-        year = agency['yearEstablished']
-        company_size = agency['companySize']
-
         # Create the insert for the company in the Companies table and add it
-        company_insert = Companies(company_name=company_name, logo_image_src=logo_image_src, city_id=city_id,
-                                   website=website, year=year, company_size=company_size)
+        company_insert = Companies(company_name=agency['name'], logo_image_src=agency['eguideImageSrc'], city_id=city_id,
+                                   website=agency['website'], year=agency['yearEstablished'], company_size=agency['companySize'])
         db.session.add(company_insert)
         db.session.commit()
 
         # Insert meta information with insert_meta() function
-        insert_meta(agency['disciplines'], 'Discipline')
-        insert_meta(agency['branches'], 'Branch')
-        insert_meta(agency['tags'], 'Tag')
+        insert_meta(agency['disciplines'], 'Discipline', agency['id'])
+        insert_meta(agency['branches'], 'Branch', agency['id'])
+        insert_meta(agency['tags'], 'Tag', agency['id'])
 
 # Closing the file
 file.close()
