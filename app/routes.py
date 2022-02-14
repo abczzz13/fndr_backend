4a6fbd38897b2336965cdfc9b09b935c55bb1b37
Thomas de Jong <thomas@tdejong.io>
2022-02-14 14:50:05 +0100

    Created first API route to GET all companies

--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,6 @@
 from flask import jsonify
 from app import app
-# from models import Companies
+from app.models import Companies, Cities, Meta, companies_meta
 
 
 @app.route("/")
@@ -10,6 +10,44 @@ def index():
 
 
 # Beginning of API
+@app.route('/companies', methods=['GET'])
+def get_all_companies():
+    all_companies = Companies.query.all()
+    output = []
+    for company in all_companies:
+        current_company = {}
+        current_company["company_id"] = company.company_id
+        current_company["company_name"] = company.company_name
+        current_company["logo_image_src"] = company.logo_image_src
+        current_company["website"] = company.website
+        current_company["year"] = company.year
+        current_company["company_size"] = company.company_size.value
+
+        city = Cities.query.filter_by(city_id=company.city_id).first()
+        current_company["city_name"] = city.city_name
+        current_company["region"] = city.region.value
+
+        current_company['disciplines'] = []
+        current_company['branches'] = []
+        current_company['tags'] = []
+
+        # TODO: Findout if we can make these queries easier and more performant
+        metas = Meta.query.join(Meta.company).filter_by(
+            company_id=company.company_id).all()
+        for meta in metas:
+            query = Meta.query.filter_by(meta_id=meta.meta_id).first()
+
+            if query.type.value == "Discipline":
+                current_company['disciplines'].append(query.meta_string)
+            if query.type.value == "Branch":
+                current_company['branches'].append(query.meta_string)
+            if query.type.value == "Tag":
+                current_company['tags'].append(query.meta_string)
+
+        output.append(current_company)
+    return jsonify(output)
+
+
 '''
 @app.route('/companies/<int:id>', methods=['GET'])
 def get_company(id):
--- env_var_script.py
+++ env_var_script.py
@@ -3,21 +3,15 @@ import os
 
 # Temporary script to set the environment variables
 # Type: python env_var_script.py
-os.environ["FLASK_APP"] = "fndr_backend.py"
-os.environ["FLASK_ENV"] = "development"
-os.environ["APP_SETTINGS"] = "config.DevelopmentConfig"
-os.environ["POSTGRES_USER"] = "postgres"
-os.environ["POSTGRES_PW"] = "aiYpDytgn8MwXEDcQvkU"
-os.environ["POSTGRES_URL"] = "localhost:5432"
-os.environ["POSTGRES_DB"] = "fndr_backend_dev"
 
-print("Environment variables for fndr_backend set")
 
 # Environment Variables from .env:
-# FLASK_APP=fndr_backend.py
-# FLASK_ENV=development
-# APP_SETTINGS=config.DevelopmentConfig
-# POSTGRES_USER="postgres"
-# POSTGRES_PW="aiYpDytgn8MwXEDcQvkU"
-# POSTGRES_URL="localhost:5432"
-# POSTGRES_DB="fndr_backend_dev"
+'''
+export FLASK_APP=fndr_backend.py
+export FLASK_ENV=development
+export APP_SETTINGS=config.DevelopmentConfig
+export POSTGRES_USER="postgres"
+export POSTGRES_PW="aiYpDytgn8MwXEDcQvkU"
+export POSTGRES_URL="localhost:5432"
+export POSTGRES_DB="fndr_backend_dev"
+'''
