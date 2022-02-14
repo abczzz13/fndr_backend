cc6ec728ffc1c3edc3aafe10a6f85b7ca3c49d34
Thomas de Jong <thomas@tdejong.io>
2022-02-14 12:41:49 +0100

    Handle duplicates companies_meta to get import script working, still needs to be optimised

--- import_data_v2.py
+++ import_data_v2.py
@@ -3,9 +3,10 @@ from app import db
 from app.models import Companies, Cities, Meta, companies_meta
 
 
-# What to do with data validation?
+# What to do with data validation? For SQL injections, SQLAlchemy takes care of that as long as you don't use any SQL syntax. In this case only SQL is used for the many-to-many companies_meta table, which is filled with Foreign Key id's created by the DB itself.
 # What to do with errors?
 # Maybe truncate DB before running script? Possibly with a check Y/N?
+# Think about precautions when importing big files?
 
 
 # Function to insert meta data
@@ -26,21 +27,24 @@ def insert_meta(input, type, company_id):
                 db.session.commit()
 
                 # Query the newly added meta string from the Meta table
-                item_check = Meta.query.filter_by(
-                    type=type, meta_string=item).first()
+                item_check = item_input
 
             # Get the meta_id for the companies_meta table if the meta string was already in the table or just added to the table
             meta_id = item_check.meta_id
 
             # Add the company_id and meta_id to the companies_meta table
-            meta_input = f'INSERT INTO companies_meta (meta_id, company_id) VALUES ({meta_id}, {company_id})'
-            db.session.execute(meta_input)
-            db.session.commit()
+            try:
+                meta_input = f'INSERT INTO companies_meta (meta_id, company_id) VALUES ({meta_id}, {company_id}) ON CONFLICT DO NOTHING'
+                db.session.execute(meta_input)
+                db.session.commit()
+            except:
+                print(
+                    f"Company ID ({company_id}) has duplicate meta ({meta_id})")
     return
 
 
 # Variables
-import_file = 'test.json'
+import_file = 'db.json'
 
 # Opening JSON file
 with open(import_file) as file:
@@ -65,8 +69,7 @@ with open(import_file) as file:
             db.session.commit()
 
             # Query the newly added city from the Cities table
-            city_check = Cities.query.filter_by(
-                city_name=agency['city']).first()
+            city_check = city_insert
 
         # Get the city_id for the Companies table if the city was already in the table or just added to the table
         city_id = city_check.city_id
@@ -78,9 +81,10 @@ with open(import_file) as file:
         db.session.commit()
 
         # Insert meta information with insert_meta() function
-        insert_meta(agency['disciplines'], 'Discipline', agency['id'])
-        insert_meta(agency['branches'], 'Branch', agency['id'])
-        insert_meta(agency['tags'], 'Tag', agency['id'])
+        insert_meta(agency['disciplines'], 'Discipline',
+                    company_insert.company_id)
+        insert_meta(agency['branches'], 'Branch', company_insert.company_id)
+        insert_meta(agency['tags'], 'Tag', company_insert.company_id)
 
 # Closing the file
 file.close()
