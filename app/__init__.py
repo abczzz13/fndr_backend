68c642f3e513e479f6efe1c4d2cf8e7161d4a2db
Thomas de Jong <thomas@tdejong.io>
2022-02-17 14:46:32 +0100

    Added a logging to file feature

--- app/__init__.py
+++ app/__init__.py
@@ -1,9 +1,10 @@
 import os
+import logging
 from flask import Flask, current_app
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from config import Config
-# from logging.handlers import RotatingFileHandler
+from logging.handlers import RotatingFileHandler
 import pdb
 # from flask_restful import Resource, Api
 
@@ -34,6 +35,22 @@ def create_app(config_class=Config):
     from app.main import bp as main_bp
     app.register_blueprint(main_bp)
 
+    # Logging to file:
+    if not app.debug and not app.testing:
+        if not os.path.exists('logs'):
+            os.mkdir('logs')
+        file_handler = RotatingFileHandler('logs/fndr_backend.log',
+                                           maxBytes=10240, backupCount=10)
+        file_handler.setFormatter(logging.Formatter(
+            '%(asctime)s %(levelname)s: %(message)s '
+            '[in %(pathname)s:%(lineno)d]'))
+        file_handler.setLevel(logging.INFO)
+        app.logger.addHandler(file_handler)
+
+        app.logger.setLevel(logging.INFO)
+        app.logger.info('FNDR startup')
+
+    # Returning app with context
     with app.app_context():
         from app import models
 
