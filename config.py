330125588e4df2a2eab6ae0d889644a6232e7843
Thomas de Jong <thomas@tdejong.io>
2022-02-22 14:24:37 +0100

    Changes to Redis variables in config for deployment to heroku v2

--- config.py
+++ config.py
@@ -53,7 +53,7 @@ class DevelopmentConfig(Config):
     # CACHE_REDIS_HOST = os.environ['CACHE_REDIS_HOST']
     # CACHE_REDIS_PORT = os.environ['CACHE_REDIS_PORT']
     # CACHE_REDIS_DB = os.environ['CACHE_REDIS_DB']
-    CACHE_REDIS_URL = os.environ['CACHE_REDIS_URL']
+    CACHE_REDIS_URL = os.environ['REDIS_URL']
     CACHE_DEFAULT_TIMEOUT = os.environ['CACHE_DEFAULT_TIMEOUT']
 
     DEBUG = True
