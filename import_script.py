023da6297f1b498ea095b56afa2f9c4d84cbae31
Thomas de Jong <thomas@tdejong.io>
2022-02-22 16:26:17 +0100

    change to import_script v3

--- import_script.py
+++ import_script.py
@@ -1,7 +1,7 @@
 from app.import_data_v2 import import_data
 from flask import app
 from app import create_app, db
-from fndr_backend.config import DevelopmentConfig
+from config import DevelopmentConfig
 from app.models import Companies, Cities, Meta, companies_meta
 
 app = create_app(config_class=DevelopmentConfig)
