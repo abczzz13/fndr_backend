ae04a635427fba682d743c799aa83ea212ac618b
Thomas de Jong <thomas@tdejong.io>
2022-03-01 16:36:26 +0100

    Removed a pdb.trace() from test_get_companies

--- tests/functional/test_get_companies.py
+++ tests/functional/test_get_companies.py
@@ -1,5 +1,4 @@
 from flask import json
-import pdb
 
 
 def test_get_companies(client, init_testdb, insert_data_db):
@@ -8,7 +7,6 @@ def test_get_companies(client, init_testdb, insert_data_db):
     WHEN the '/api/companies' page is requested (GET)
     THEN the response will contain the first 15 records and their associated data
     '''
-    pdb.set_trace()
     response = client.get('/api/v1/companies')
 
     data = json.loads(response.get_data(as_text=True))
