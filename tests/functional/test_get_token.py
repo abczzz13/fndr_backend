188562f04573d2f24d06f4c54bceaa20ab865abc
Thomas de Jong <thomas@tdejong.io>
2022-03-04 16:57:35 +0100

    Added a test to make sure no token was returned with invalid user credentials

--- tests/functional/test_get_token.py
+++ tests/functional/test_get_token.py
@@ -4,8 +4,8 @@ from flask import json
 def test_get_token(client, new_user):
     '''
     GIVEN a Flask application configured for testing
-    WHEN a POST request is made to /api/v1/companies with a valid company
-    THEN check that the response is valid and particular data can be found
+    WHEN a POST request is made to /api/v1/token with valid user credentials
+    THEN check that a token is returned
     '''
 
     data = {
@@ -17,6 +17,27 @@ def test_get_token(client, new_user):
                            headers={"Content-Type": "application/json"},)
 
     result = json.loads(response.get_data(as_text=True))
-    print(result)
+
     assert response.status_code == 200
     assert result['token'] is not None
+
+
+def test_get_token_invalid(client, new_user):
+    '''
+    GIVEN a Flask application configured for testing
+    WHEN a POST request is made to /api/v1/token with invalid user credentials
+    THEN check that the response is unauthorized and correct error message is shown
+    '''
+
+    data = {
+        "username": "Not existing User",
+        "password": "testtest"
+    }
+
+    response = client.post("/api/v1/token", data=json.dumps(data),
+                           headers={"Content-Type": "application/json"},)
+
+    result = json.loads(response.get_data(as_text=True))
+
+    assert response.status_code == 401
+    assert result['message'] == 'Invalid user credentials'
