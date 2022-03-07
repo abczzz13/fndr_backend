22dce416447ebd14d8d478825f468a4252eb2a3b
Thomas de Jong <thomas@tdejong.io>
2022-03-07 17:04:13 +0100

    Fixed another import bug in fndr_backened.py

--- fndr_backend.py
+++ fndr_backend.py
@@ -1,17 +1,15 @@
-#from app import app, db
 from app import create_app, db
-from app.models import Companies, Cities, Meta, Users, companies_meta, CompaniesSchema, MetaSchema
-import pdb
-import os
+from app.models import Companies, Cities, Meta, Users, companies_meta
 import config
+import os
+
 
 config_variables = config.DevelopmentConfig
 if os.environ.get('FLASK_ENV') == 'production':
     config_variables = config.ProductionConfig
-# pdb.set_trace()
 app = create_app(config_class=config_variables)
 
 
 @app.shell_context_processor
 def make_shell_context():
-    return {'db': db, 'Companies': Companies, 'Cities': Cities, 'Meta': Meta, 'companies_meta': companies_meta, 'Users': Users, 'CompaniesSchema': CompaniesSchema, 'MetaSchema': MetaSchema}
+    return {'db': db, 'Companies': Companies, 'Cities': Cities, 'Meta': Meta, 'companies_meta': companies_meta, 'Users': Users}
